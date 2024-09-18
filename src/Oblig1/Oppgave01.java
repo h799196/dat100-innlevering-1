1f8018c0af240e95420f33f5ce556426704999a2
h799196 <799196@stud.hvl.no>
2024-09-18 14:19:20 +0200

    Update Oppgave01.java

--- src/Oblig1/Oppgave01.java
+++ src/Oblig1/Oppgave01.java
@@ -6,31 +6,41 @@ public class Oppgave01 {
 
 	public static void main(String[] args) {
 		
+		int i;
+		
+		for (i=0;i<=10;i++) {
+	
 		String melding = "Skriv inn poengsum 0-100 ";
 		
 		int poengsum = Integer.parseInt(JOptionPane.showInputDialog(melding));
 		
-		if (poengsum < 0 && poengsum >100) {
-			System.out.print("Ugyldig verdi");}
-			
+		if (poengsum < 0){
+			System.out.println("Ugyldig verdi");}
+		
+		else if (poengsum > 100) {
+			System.out.println("Ugyldig verdi");}
+		
 		else if (poengsum<39) {	
-			System.out.print("karakter F ");
+			System.out.println("karakter F ");
 		}
 		else if (poengsum<49) {
-			System.out.print("karakter E ");
+			System.out.println("karakter E ");
 		}
 		else if (poengsum<59) {
-			System.out.print("karakter D ");
+			System.out.println("karakter D ");
 		}
 		else if (poengsum<79) {
-			System.out.print("karakter C ");
+			System.out.println("karakter C ");
 		}
 		else if (poengsum<89) {
-			System.out.print("karakter B ");
+			System.out.println("karakter B ");
 		}
 		else
-			System.out.print("karakter A ");
+			System.out.println("karakter A ");
 		}
 	}
+	}
+	
+	
 
 
